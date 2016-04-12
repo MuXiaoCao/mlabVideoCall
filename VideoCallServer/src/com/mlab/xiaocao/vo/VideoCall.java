707aaea02899aa350d0d12e37d215faac253b469
muxiaocao <415804388@qq.com>
2016-04-12 20:45:12 +0800

    20160412

--- VideoCallServer/src/com/mlab/xiaocao/vo/VideoCall.java
+++ VideoCallServer/src/com/mlab/xiaocao/vo/VideoCall.java
@@ -3,15 +3,26 @@ package com.mlab.xiaocao.vo;
 import java.util.ArrayList;
 
 
-
 public class VideoCall {
 	
-	private ClientBean srcUser;
-	private ClientBean targetUser;
+	private User srcUser;
+	private User targetUser;
 
-	
+	public VideoCall(String srcUserID,String targetUserID) {
+		srcUser = new User(srcUserID);
+		targetUser = new User(targetUserID);
+	}
 	
 	class User {
+		
+		public User(String id) {
+			this.ID = id;
+			receiveAudioTimeStmp = new ArrayList<Long>();
+			receiveVideoTimeStmp = new ArrayList<Long>();
+			sendAudioTimeStmp = new ArrayList<Long>();
+			sendVideoTimeVideoStmp = new ArrayList<Long>();
+		}
+		
 		
 		private String ID;
 		private ArrayList<Long> receiveVideoTimeStmp;
